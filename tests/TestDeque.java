03e02c449fefd484ce7fd69a72681ddc437f7a5e
K1ta <kobalt31@mail.ru>
2018-01-17 15:14:40 +0300

    Add tests

--- tests/TestDeque.java
+++ tests/TestDeque.java
@@ -1,6 +1,7 @@
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Iterator;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -49,4 +50,153 @@ public class TestDeque {
         Assert.assertEquals(deque.size(), 0);
     }
 
+    @Test
+    public void testSize() {
+        deque.pushFront(2);
+        deque.pushFront(1);
+        deque.pushBack(3);
+        Assert.assertEquals(3, deque.size());
+    }
+
+    @Test
+    public void testSizeAfterPushAndPop() {
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 == 0) deque.pushFront(i);
+            else deque.pushBack(i);
+        }
+        for (int i = 0; i < 50; i++) {
+            if (i % 2 == 0) deque.popBack();
+            else deque.popFront();
+        }
+        Assert.assertEquals(50, deque.size());
+    }
+
+    @Test
+    public void testOnePushFront() {
+        deque.pushFront(1);
+        Integer expected = 1;
+        Assert.assertEquals(1, deque.size());
+        Assert.assertEquals(expected, deque.popFront());
+    }
+
+    @Test
+    public void testThreePushFront() {
+        deque.pushFront(1);
+        deque.pushFront(100);
+        deque.pushFront(1000);
+        Integer expected = 1000;
+        Assert.assertEquals(3, deque.size());
+        Assert.assertEquals(expected, deque.popFront());
+    }
+
+    @Test
+    public void testHundredPushFront() {
+        for (int i = 0; i < 100; i++) {
+            deque.pushFront(i);
+        }
+        Assert.assertEquals(100, deque.size());
+        Integer expected = 99;
+        Assert.assertEquals(expected, deque.popFront());
+    }
+
+    @Test
+    public void testOnePushBack() {
+        deque.pushBack(1);
+        Integer expected = 1;
+        Assert.assertEquals(1, deque.size());
+        Assert.assertEquals(expected, deque.popBack());
+    }
+
+    @Test
+    public void testThreePushBack() {
+        deque.pushBack(1);
+        deque.pushBack(100);
+        deque.pushBack(1000);
+        Integer expected = 1000;
+        Assert.assertEquals(3, deque.size());
+        Assert.assertEquals(expected, deque.popBack());
+    }
+
+    @Test
+    public void testHundredPushBack() {
+        for (int i = 0; i < 100; i++) {
+            deque.pushBack(i);
+        }
+        Assert.assertEquals(100, deque.size());
+        Integer expected = 99;
+        Assert.assertEquals(expected, deque.popBack());
+    }
+
+    @Test
+    public void testEmptyPopFront() {
+        Assert.assertNull(deque.popFront());
+    }
+
+    @Test
+    public void testThreePopFront() {
+        deque.pushBack(1);
+        deque.pushBack(2);
+        deque.pushBack(3);
+        Integer expected1 = 1;
+        Integer expected2 = 2;
+        Integer expected3 = 3;
+        Assert.assertEquals(expected1, deque.popFront());
+        Assert.assertEquals(expected2, deque.popFront());
+        Assert.assertEquals(expected3, deque.popFront());
+        Assert.assertNull(deque.popFront());
+    }
+
+    @Test
+    public void testHundredPopFront() {
+        for (int i = 0; i < 100; i++) {
+            deque.pushBack(i);
+        }
+        Integer expected = 0;
+        Assert.assertEquals(expected, deque.popFront());
+    }
+
+    @Test
+    public void testEmptyPopBack() {
+        Assert.assertNull(deque.popBack());
+    }
+
+    @Test
+    public void testThreePopBack() {
+        deque.pushFront(1);
+        deque.pushFront(2);
+        deque.pushFront(3);
+        Integer expected1 = 1;
+        Integer expected2 = 2;
+        Integer expected3 = 3;
+        Assert.assertEquals(expected1, deque.popBack());
+        Assert.assertEquals(expected2, deque.popBack());
+        Assert.assertEquals(expected3, deque.popBack());
+        Assert.assertNull(deque.popBack());
+    }
+
+    @Test
+    public void testHundredPopBack() {
+        for (int i = 0; i < 100; i++) {
+            deque.pushFront(i);
+        }
+        Integer expected = 0;
+        Assert.assertEquals(expected, deque.popBack());
+    }
+
+    @Test
+    public void testLinkedDequeIterator() {
+        if (deque.getClass() == CyclicArrayDeque.class) {
+            return;
+        }
+        deque.pushFront(2);
+        deque.pushFront(1);
+        deque.pushBack(3);
+        deque.pushBack(4);
+        Iterator<Integer> it = deque.iterator();
+        Assert.assertTrue(it.hasNext());
+        Assert.assertEquals((Integer) 1, it.next());
+        Assert.assertEquals((Integer) 2, it.next());
+        Assert.assertEquals((Integer) 3, it.next());
+        Assert.assertEquals((Integer) 4, it.next());
+    }
 }
--- tests/TestPriorityQueue.java
+++ tests/TestPriorityQueue.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import seminar1.collections.ArrayPriorityQueue;
 import seminar1.collections.IPriorityQueue;
 
+import java.util.NoSuchElementException;
+
 /**
  * Класс тестирующий интерфейс {@link IPriorityQueue<Integer>} на основе {@link ArrayPriorityQueue<>}
  */
@@ -28,5 +30,135 @@ public class TestPriorityQueue {
         queue.add(0);
         Assert.assertFalse(queue.isEmpty());
         Assert.assertEquals(queue.size(), 1);
+    }
+
+    @Test
+    public void testOneAdd() {
+        queue.add(0);
+        Integer expected = 0;
+        Assert.assertEquals(expected, queue.peek());
+    }
+
+    @Test
+    public void testTwoAdd() {
+        queue.add(2);
+        queue.add(1);
+        Integer expected = 1;
+        Assert.assertEquals(expected, queue.peek());
+    }
+
+    @Test
+    public void testThreeAdd() {
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        Integer expected1 = 1;
+        Integer expected2 = 2;
+        Assert.assertEquals(expected1, queue.extractMin());
+        Assert.assertEquals(expected2, queue.extractMin());
+    }
+
+    @Test
+    public void testTenAdd() {
+        for(int i = 0; i < 10; i++) {
+            queue.add(i);
+        }
+        Integer expected = 0;
+        Assert.assertEquals(expected, queue.peek());
+    }
+
+    @Test
+    public void testElevenAdd() {
+        for(int i = 0; i < 11; i++) {
+            queue.add(i);
+        }
+        Integer expected = 0;
+        Assert.assertEquals(expected, queue.peek());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testEmptyQueueAdd() {
+        queue.add(null);
+    }
+
+    @Test
+    public void testPeek() {
+        Integer min = 100;
+        for(int i = 0; i < 100; i++) {
+            Integer key = (int)(Math.random() * 100);
+            queue.add(key);
+            if(key < min) min = key;
+        }
+        Assert.assertEquals(min, queue.peek());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testEmptyQueuePeek() {
+        queue.peek();
+    }
+
+    @Test
+    public void testOneExtractMin() {
+        queue.add(1);
+        Integer expected = 1;
+        Assert.assertEquals(expected, queue.extractMin());
+    }
+
+    @Test
+    public void testTenExtractMin() {
+        for(int i = 0; i < 10; i++) {
+            Integer key = (int)(Math.random() * 100);
+            queue.add(key);
+        }
+        Integer prev = -1;
+        for(int i = 0; i < 10; i++) {
+            Integer key = queue.extractMin();
+            Assert.assertTrue(prev <= key);
+            prev = key;
+        }
+    }
+
+    @Test
+    public void testHundredExtractMin() {
+        for(int i = 0; i < 100; i++) {
+            Integer key = (int)(Math.random() * 100);
+            queue.add(key);
+        }
+        Integer prev = -1;
+        for(int i = 0; i < 100; i++) {
+            Integer key = queue.extractMin();
+            Assert.assertTrue(prev <= key);
+            prev = key;
+        }
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testEmptyQueueExtractMin() {
+        queue.extractMin();
+    }
+
+    @Test
+    public void testOneSize() {
+        queue.add(1);
+        Assert.assertEquals(1, queue.size());
+    }
+
+    @Test
+    public void testTenSize() {
+        for(int i = 0; i < 11; i++) {
+            queue.add(i);
+        }
+        Assert.assertEquals(11, queue.size());
+    }
+
+    @Test
+    public void testSize() {
+        for(int i = 0; i < 20; i++) {
+            queue.add(i);
+        }
+        for(int i = 0; i < 15; i++) {
+            queue.extractMin();
+        }
+        Assert.assertEquals(5, queue.size());
     }
 }
